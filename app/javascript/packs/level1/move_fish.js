c823bdeb2da975e7114f93ae2b6cf784c2a1b5ea
Ryosuke <tffcd0514@gmail.com>
2022-04-01 17:17:00 +0900

    add: 魚の速度をランダムに設定 #2

--- app/javascript/packs/level1/move_fish.js
+++ app/javascript/packs/level1/move_fish.js
@@ -1,16 +1,30 @@
 
 import anime from "animejs"
 
-var elem = document.getElementsByClassName('fish');
+// ####  画面高さ設定  ####
+  let vh = window.innerHeight * 0.01;
+  // カスタム変数--vhの値をドキュメントのルートに設定
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// 引数で渡した範囲内のランダムな整数を生成するための関数
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 window.onload = function() {
-  anime({
-    targets: elem,
-    translateX: 1000,
-    loop: true,
-    duration: 3000,
-    easing: 'easeInQuad'
+  var fishElements = document.querySelectorAll(".fish");
+  fishElements.forEach(fish => {
+    anime.speed = Math.random() * 0.5 + 0.5;
+    anime({
+      targets: fish,
+      translateX: 1000,
+      loop: true,
+      duration: getRandomInt(1000,5000),
+      easing: 'easeInQuad',
+      delay: getRandomInt(0,300)
+    });
   });
+  
   $('#hiramasa').on('click', function() {
     $('#angry').hide();
     $('#man').show();
@@ -19,14 +33,17 @@ window.onload = function() {
   elements.forEach(elm => {
     elm.addEventListener('click', function(){
       $('#man').hide();
-      $('#angry').show(); 
+      $('#angry').show();
+      setTimeout(function(){
+        $('#angry').hide();
+        $('#man').show();
+      },1000);
+      
     });
   });
 
-// 引数で渡した範囲内のランダムな整数を生成するための関数
-var getRandomInt = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+// ########  魚の画像要素の高さをランダムに変更  #########
+
 
 // 要素の取得
 var $container = $('.container'),
@@ -35,11 +52,11 @@ var $container = $('.container'),
   containerHeight = $container.height();
 
 
-  // liそれぞれをランダムに配置
+  // fishそれぞれをランダムに配置
 $.map($fish, function(item, index) {
   var $item = $(item), // fishのli要素が1番目から順に入ってきます
-    topPos = getRandomInt(0, containerHeight), // 0〜500の間でtopの座標を取得
-    leftPos = getRandomInt(-700, -100); // 0〜960の間でleft の座標を取得
+    topPos = getRandomInt(130, containerHeight), // 0〜500の間でtopの座標を取得
+    leftPos = getRandomInt(-700, -200); // 0〜960の間でleft の座標を取得
   // 取得したランダム座標を設定
   $item.css({
     top: topPos,
@@ -48,3 +65,6 @@ $.map($fish, function(item, index) {
 });
 
 }
+
+
+
