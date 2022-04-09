fbdc7bbb1b08dfb96a2a1c2904bb94388a9f7956
Ryosuke <tffcd0514@gmail.com>
2022-04-10 06:06:55 +0900

    add: level2のルーティングとコントローラー設定 #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'play#home'
   get 'play/level1'
+  get 'play/level2'
   get 'play/result'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
